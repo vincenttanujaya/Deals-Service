a88b26325d980b448db3e301ce57f7d7ca46a3a9
vincenttanujaya <vincent.tanujaya@gmail.com>
2019-05-03 18:52:36 +0700

    Tambah CDM PDM

---
+++ src/api/src/main/java/com/gopek/deals/DealsApplication.java
@@ -0,0 +1,13 @@
+package com.gopek.deals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealsApplication.class, args);
+	}
+
+}
